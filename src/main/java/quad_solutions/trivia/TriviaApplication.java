353d4129538499ad56afe843bdf1fdc2b3d7b0b3
Ruben Schnell <rubenschnell1805@gmail.com>
2026-09-17 16:17:23 +0200

    First commit, spring boot application

---
+++ src/main/java/quad_solutions/trivia/TriviaApplication.java
@@ -0,0 +1,13 @@
+package quad_solutions.trivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/quad_solutions/trivia/TriviaApplicationTests.java
@@ -0,0 +1,13 @@
+package quad_solutions.trivia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
